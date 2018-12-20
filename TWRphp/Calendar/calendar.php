66f37dc309ee94d4d2fb6716ab412ba8a70c921a
Sneriko <36701982+Sneriko@users.noreply.github.com>
2018-12-20 10:53:08 +0100

    Add files via upload

--- TWRphp/Calendar/calendar.php
+++ TWRphp/Calendar/calendar.php
@@ -6,13 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Tasty Recipes Webpage</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link href="mystyle.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
+    <link href="CalendarStyle.css" rel="stylesheet" type="text/css" />
 
 </head>
 
 <body>
 
-<div class="container-fluid header">
+<div class="container-fluid">
     <nav class="navbar navbar-expand-lg navbar-light">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -20,27 +20,31 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="../index.html">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="../index.php">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="Calendar/calendar.html">Calendar</a>
+                    <a class="nav-link" href="calendar.php">Calendar</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Recipes
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Swedish meatballs</a>
-                        <a class="dropdown-item" href="#">Swedish pancakes</a>
+                        <a class="dropdown-item" href="../Recipes/recipes.php?id=0">Swedish meatballs</a>
+                        <a class="dropdown-item" href="../Recipes/recipes.php?id=1">Swedish pancakes</a>
                     </div>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
                 </li>
             </ul>
         </div>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn  my-2 my-sm-0" type="submit">Search</button>
+        <form class="form-inline my-2 my-lg-0" action="login.inc.php" method="post">
+            <input class="form-control mr-sm-2" type="text" name="username" placeholder="Username">
+            <input class="form-control mr-sm-2" type="password" name="pwd" placeholder="Password">
+            <button class="btn  my-2 my-sm-0" type="submit" name="login-submit">Login</button>
+            <!--<button class="btn  my-2 my-sm-0" type="submit" name="signup-submit">Sign up</button>-->
+        </form>
+        <button class="btn  my-2 my-sm-0"><a href="signup.php">Sign up</a></button>
+        <form class="form-inline my-2 my-lg-0" action="includes/logout.inc.php" method="post">
+            <button class="btn  my-2 my-sm-0" type="submit" name="logout-submit">Logout</button>
         </form>
     </nav>
 </div>
--- TWRphp/Recipes/recipes.php
+++ TWRphp/Recipes/recipes.php
@@ -8,7 +8,7 @@ $_SESSION['recipe'] = $_GET['id'];
 <html lang="en">
 <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="RecipeStyle.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tasty Web Recipes</title>
@@ -17,6 +17,44 @@ $_SESSION['recipe'] = $_GET['id'];
 </head>
 
 <body>
+
+<div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/index.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../Calendar/calendar.php">Calendar</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Recipes
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="recipes.php?id=0">Swedish meatballs</a>
+                        <a class="dropdown-item" href="recipes.php?id=1">Swedish pancakes</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <form class="form-inline my-2 my-lg-0" action="login.inc.php" method="post">
+            <input class="form-control mr-sm-2" type="text" name="username" placeholder="Username">
+            <input class="form-control mr-sm-2" type="password" name="pwd" placeholder="Password">
+            <button class="btn  my-2 my-sm-0" type="submit" name="login-submit">Login</button>
+            <!--<button class="btn  my-2 my-sm-0" type="submit" name="signup-submit">Sign up</button>-->
+        </form>
+        <button class="btn  my-2 my-sm-0"><a href="signup.php">Sign up</a></button>
+        <form class="form-inline my-2 my-lg-0" action="includes/logout.inc.php" method="post">
+            <button class="btn  my-2 my-sm-0" type="submit" name="logout-submit">Logout</button>
+        </form>
+    </nav>
+</div>
+
 <?php
 
 $var = $_GET["id"];
@@ -77,7 +115,7 @@ while($row = mysqli_fetch_assoc($result)){
         echo "<div style='color: blue'>".$row['username']."</div>";
         echo "<div style='border: 1px solid black; width: 50%; height: auto'>" . $comment . "</div>";
         if ($_SESSION['username'] == $row['username']) {
-            echo "<a href='delete-entry.php?id=$idcomment'><button>Delete</button></a>";
+            echo "<a  style='color: beige' href='delete-entry.php?id=$idcomment'><button style='background-color: beige'>Delete</button></a>";
         }
 
 
@@ -86,5 +124,10 @@ while($row = mysqli_fetch_assoc($result)){
 echo $row['comment'];
 
 ?>
-    </body>
-    </html>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</body>
+</html>
